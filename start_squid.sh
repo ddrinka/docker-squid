9a2650e5e2241ba0630a596b5aedb979f43c6227
Douglas Drinka <ddrinka@gmail.com>
2015-11-17 05:02:17 +0000

    Added syslog logging device to startup command and daemonized

--- start_squid.sh
+++ start_squid.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --publish 3128:3128 --volume /srv/docker/squid/etc/squid.conf:/etc/squid/squid.conf --volume /srv/docker/squid/etc/certs:/etc/squid/certs --volume /srv/docker/squid/etc/passwords:/etc/squid/passwords squid squid -NYCd 1
+docker run --restart=always -d --publish 3128:3128 --volume /dev/log:/dev/log --volume /srv/docker/squid/etc/squid.conf:/etc/squid/squid.conf --volume /srv/docker/squid/etc/certs:/etc/squid/certs --volume /srv/docker/squid/etc/passwords:/etc/squid/passwords squid squid -NYCd 1
